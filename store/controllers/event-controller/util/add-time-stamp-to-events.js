a1a2bea2490fbde90f3e5134f02bdeb948f76d5a
James Cox <j-cox03@hotmail.com>
2018-03-18 19:58:46 +0000

    Changed how timeStamp works

--- store/controllers/event-controller/util/add-time-stamp-to-events.js
+++ store/controllers/event-controller/util/add-time-stamp-to-events.js
@@ -1,9 +1,25 @@
 
 function addTimeStampToEvents(events){
   return events.map((event) => {
-    event.timeStamp = new Date().toString()
+    event.timeStamp = timeStamp()
     return event
   })
+}
+
+function timeStamp(){
+    const date = new Date()
+
+    const year = date.getFullYear()
+    const month = formatDateDigits(date.getMonth())
+    const day = formatDateDigits(date.getDate())
+    const time = date.getTime()
+
+    return `${year}.${month}.${day}.${time}`
+}
+
+function formatDateDigits(number){
+  const numAsString = number.toString()
+  return numAsString.length === 1 ? `0${numAsString}` : numAsString
 }
 
 module.exports = addTimeStampToEvents
