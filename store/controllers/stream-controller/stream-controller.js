37f39ef0f480e9b7be2e2cd7e3e7581b3f4737a2
James Cox <j-cox03@hotmail.com>
2018-03-25 19:23:46 +0100

    renamed file streams -> streamNames

--- store/controllers/stream-controller/stream-controller.js
+++ store/controllers/stream-controller/stream-controller.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const streamNames = require("./util/streams")
+const streamNames = require("./util/streamNames")
 
 router.get('/', async (req, res) => {
   res.json(await streamNames())
--- store/controllers/stream-controller/util/streamNames.js
+++ store/controllers/stream-controller/util/streamNames.js
@@ -4,9 +4,11 @@ const connect = require("./../../../mongo-db/connect")
 async function streamNames(){
   const {database, client} = await connect()
   const collections = await database.collections(null)
-  return collections.map((collection) => {
-    return collection.s.name
-  })
+  return collections.map(extractStreamName)
+}
+
+function extractStreamName({s: {name: streamName}}){
+  return streamName
 }
 
 module.exports = streamNames
